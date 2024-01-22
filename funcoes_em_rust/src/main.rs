3a470d7eb9340fa3d720d302b8b430b821e12959
carlosismaelad <carlosismaelad@outlook.com>
2024-01-22 00:31:17 -0300

    Casting no retorno de uma funcao.

--- funcoes_em_rust/src/main.rs
+++ funcoes_em_rust/src/main.rs
@@ -11,15 +11,22 @@ fn maior_menor_que(a: i32, b: i32) -> i32 {
 }
 
 fn alguma_fn(a: f32, b: f32) -> f32 {
-    println!("Essa função devolve um float. O Resultado é: ");
+    print!("Essa função devolve um float. O Resultado é: ");
     a / b
     // usamos uma "Expressão de retorno implícito" onde,
     // a última expressão repassada sem ";" é entendida
     // como o retorno da função
 }
 
+fn casting_no_retorno() -> f32 {
+    10 as f32
+    // pedimos o retorno do número 10 como um float de 32 bits
+    // com esta sintaxe ocorrerá um casting do retorno
+}
+
 fn main() {
     println!("O dobro do número 5 é {}.", dobro(5));
     println!("O maior número entre 5 e 9 é {}.", maior_menor_que(5, 9));
     println!("{:.2}", alguma_fn(12.2, 2.2));
+    println!("A divisão de 20 por 2 é {:.2}", casting_no_retorno());
 }
