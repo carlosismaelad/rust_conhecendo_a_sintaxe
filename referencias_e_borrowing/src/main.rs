c37b5651f5db828abebd9c717ff37f00ec1f018a
carlosismaelad <carlosismaelad@outlook.com>
2024-01-26 11:08:15 -0300

    Como repassar apenas uma referência de algo em vez de dar o valor

--- referencias_e_borrowing/src/main.rs
+++ referencias_e_borrowing/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
 // Uma forma de como você poderia definir e usar uma função
 // calcula_tamanho que recebe uma referência para um objeto como
 // parâmetro, em vez de pegar este valor para si:
+// O "&" são referencias e ele permite que você se refira a algum
+// valor sem tomar ele para si.
 fn calcula_tamanho(s: &String) -> usize {
     s.len()
 }
